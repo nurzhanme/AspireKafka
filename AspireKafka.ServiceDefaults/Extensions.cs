7f3b77484b92f560ebe812c3b7c355c0cd8e8fb1
Nurzhan <nurzhait@gmail.com>
2025-03-07 18:01:01 +0500

    add tracings

--- AspireKafka.ServiceDefaults/Extensions.cs
+++ AspireKafka.ServiceDefaults/Extensions.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.ServiceDiscovery;
+using Npgsql;
 using OpenTelemetry;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Trace;
@@ -62,7 +63,10 @@ public static class Extensions
                     .AddAspNetCoreInstrumentation()
                     // Uncomment the following line to enable gRPC instrumentation (requires the OpenTelemetry.Instrumentation.GrpcNetClient package)
                     //.AddGrpcClientInstrumentation()
-                    .AddHttpClientInstrumentation();
+                    .AddHttpClientInstrumentation()
+                    .AddEntityFrameworkCoreInstrumentation()
+                    .AddNpgsql()
+                    ;
             });
 
         builder.AddOpenTelemetryExporters();
